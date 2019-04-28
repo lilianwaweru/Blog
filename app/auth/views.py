124921f5a32f74f0077d9503f44fb8c9cce6915d
lilianwaweru <lilowesh.lw@gmail.com>
2019-04-28 20:15:15 +0300

    creadted edit button for profile

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,10 @@
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,flash,request
 from ..models import User
-from .forms import RegistrationForm
+from .forms import LoginForm,RegistrationForm
+from flask_login import login_user,logout_user,login_required
 from .. import db
-
+from ..email import mail_message
+from . import auth
 
 
 @auth.route('/login')

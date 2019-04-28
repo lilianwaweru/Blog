c791f9e28433f64bda20123ee6430c62b88861b2
lilianwaweru <lilowesh.lw@gmail.com>
2019-04-28 20:31:34 +0300

    added mail set up for server and configure the port to use the gmail smtp server port

--- app/__init__.py
+++ app/__init__.py
@@ -35,7 +35,7 @@ def create_app(config_name):
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
     # configure UploadSet
-    # configure_uploads(app,photos)
+    configure_uploads(app,photos)
 
     return app
 

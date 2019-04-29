9a505df4bc707c7865a21d4391938930b8c5fe73
lilianwaweru <lilowesh.lw@gmail.com>
2019-04-29 08:57:45 +0300

    imported bootstrap in new_blog.html

--- manage.py
+++ manage.py
@@ -13,17 +13,16 @@ manager.add_command('server',Server)
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User,Role = Role )
+
 @manager.command
 def test():
     """Run the unit tests."""
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
-
-@manager.shell
-def make_shell_context():
-    return dict(app = app,db = db,User = User,Role = Role )
-
 
 
 if __name__ == '__main__':
